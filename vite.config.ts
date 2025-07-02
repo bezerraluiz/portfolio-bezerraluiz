4ec5ed00c844d69d084c786ea9d436eb27398a4c
Luiz Bezerra <luizantoniopcb@gmail.com>
2025-07-02 15:33:41 -0300

    refresh

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/portfolio-bezerraluiz/",
+  base: "bezerraluiz/portfolio-bezerraluiz", // https://github.com/bezerraluiz/portfolio-bezerraluiz
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
